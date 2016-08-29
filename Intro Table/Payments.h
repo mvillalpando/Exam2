d415f1123deb7bcb9be84c40659df14cfe69b83f
Carlos <carlos.V@carloss-macbook-pro.local>
2016-08-29 15:34:43 -0500

    Button "Comprar" added

---
+++ Intro Table/Payments.h
@@ -0,0 +1,13 @@
+//
+//  Payments.h
+//  Intro Table
+//
+//  Created by Carlos E. Villalpando on 8/29/16.
+//  Copyright © 2016 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Payments : UIViewController
+
+@end
